4616b48588907c612b1318d1695cc2de57d40634
Radosław Dąbrowski <radoslaw.dab.dabrowski@gmail.com>
2025-03-19 14:50:38 +0100

    Adding new Scrapy project

--- prices/models.py
+++ prices/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class ProductPrice(models.Model):
-    name = models.CharField(max_length=255)  # Nazwa produktu
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # Cena produktu, do 2 miejsc po przecinku
-    store_name = models.CharField(max_length=255)  # Nazwa sklepu
-    product_url = models.URLField()  # URL produktu w sklepie
-    date_collected = models.DateField()  # Data zebrania ceny
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    store_name = models.CharField(max_length=255)
+    product_url = models.URLField()
+    date_collected = models.DateField()
 
     def __str__(self):
         return f"{self.name} - {self.price} ({self.store_name})"
